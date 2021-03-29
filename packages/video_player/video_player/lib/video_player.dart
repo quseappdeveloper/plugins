2dd1930412c266d1a50f741cda19d3a613e2de33
Saswat <ss.saswatsahoo@gmail.com>
2021-03-29 20:54:56 +0530

    Getter added for isDisposed

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -256,6 +256,8 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   @visibleForTesting
   int get textureId => _textureId;
 
+  bool get isDisposed => _isDisposed;
+
   /// Attempts to open the given [dataSource] and load metadata about the video.
   Future<void> initialize() async {
     _lifeCycleObserver = _VideoAppLifeCycleObserver(this);
